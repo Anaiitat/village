14fd1b104efcdf8dcb5f3011920e0775c4948a72
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-09-13 21:47:37 +0200

    Insertion filterbynamepipe

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,13 +1,15 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Header } from './header/header';
 import { Home } from './home/home';
+import { FilterByNamePipe } from './filterbyname-pipe';
 
 @NgModule({
-  declarations: [App, Home, Header],
+  declarations: [App, Home, Header, FilterByNamePipe],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,17 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductsService, Product,  } from "../products.service";
+
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: false,
   template: `
     <app-header></app-header>
-    <p>
-      home works!
-    
-    </p>
+    <main>
+      <section class="description">
+        <p>
+          Redécouvrez l’univers d’Astérix et Obélix à travers nos figurines
+          artisanales, faites main avec passion. <br />Des créations uniques
+          pour les nostalgiques des célèbres Gaulois !
+        </p>
+      </section>
+      <section class="produit">
+        <div>
+          <h1>Produits</h1>
+          <div>
+            <input
+              type="text"
+              placeholder="Rechercher par nom"
+              (input)="onSearch($event)"
+              [value]="searchText"
+            />
+            <div *ngFor="let product of products | filterByName : searchText">
+              <h2>{{ product.name }}</h2>
+              <img [src]="product.imageUrl" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   `,
-  styles: ``
-})
-export class Home {
+  styles: `
+  
+  .description{
+    display: flex;
+    justify-content: center;
+    padding: 15px
+  }
 
+  .description p {
+    text-align: center;
+    font-size : 22px;
+    font-family: 'Bubble '
+  }
+
+  `,
+})
+export class Home implements OnInit {
+  products: Product[] = [];
+  constructor(private productsService: ProductsService) {}
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+  }
+  onSearch(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.searchText = target.value;
+  }
+
+  searchText: string = "";
 }
