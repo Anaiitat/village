e3c74a65ea0a2b3976f00d3ff70a88fe20fd64d4
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-09-23 17:22:19 +0200

    ajout component produit

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home'
 import { About } from "./about/about";
 import { Contact } from "./contact/contact";
+import { Produit } from "./produit/produit";
 
 const routes: Routes = [
   { path: "", component: Home },
   { path: "about", component: About },
   { path: "contact", component: Contact },
+  { path: "produit", component: Produit },
 ];
 
 @NgModule({
---
+++ src/app/produit/produit.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  standalone: false,
+  template: `
+    <p>
+      produit works!
+    </p>
+  `,
+  styles: ``
+})
+export class Produit {
+
+}
