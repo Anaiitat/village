f0a07a73a43328310d5a34b44c2ff06af6f1427b
Anaiitat <73583249+Anaiitat@users.noreply.github.com>
2025-09-23 14:38:19 +0200

    finition footer

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   standalone: false,
   template: `
-    <p>
-      footer works!
-    </p>
+    <footer>
+      <div class="logo">
+        <a href="https://www.instagram.com/" target="_blank"
+          ><img src="./assets/images/logo/instagram.png"
+        /></a>
+        <a href="https://www.facebook.com/" target="_blank"
+          ><img src="./assets/images/logo/facebook.png"
+        /></a>
+        <a href="https://www.x.com/" target="_blank"
+          ><img src="./assets/images/logo/twitter.png"
+        /></a>
+      </div>
+    </footer>
   `,
-  styles: ``
-})
-export class Footer {
-
+  styles: `
+  
+footer{
+  background-color: #F9D0A3;
+  padding:10px;
+  margin-top:50px
 }
+
+.logo{
+  display:flex;
+  justify-content:center;
+  padding:20px;
+}
+
+.logo a {
+  padding:10px;
+}
+
+.logo img{
+  width:35px
+}
+  
+  `,
+})
+export class Footer {}
+
